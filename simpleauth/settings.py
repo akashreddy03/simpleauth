65b0e17f106c0d48daa381c1996ccdb40f173e94
akashreddy03 <akashreddy2003@gmail.com>
2022-10-02 17:34:15 +0530

    Cleanup

--- simpleauth/settings.py
+++ simpleauth/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5xg&q#!*fl0ub&wg9i$*bx!uo0e!hcud5rn8qvv$5%a^g$!ksy'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
